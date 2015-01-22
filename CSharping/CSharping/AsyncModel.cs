0d60d26b6defcb18244c6a031dd13a02ae173e55
dominic wyler <dominic.wyler@gmail.com>
2015-01-22 07:43:42 +0100

    added tests for async, with one and two tasks

---
+++ CSharping/CSharping/AsyncModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharping
+{
+    public class AsyncModel
+    {
+        private readonly ConcurrentQueue<string> _messages = new ConcurrentQueue<string>();
+
+        public void AddMessage(string format, params object[] args)
+        {
+            _messages.Enqueue(string.Format(format, args));
+            // string.Format(" on threadId {0}", Thread.CurrentThread.ManagedThreadId)
+        }
+
+        public List<string> GetMessages()
+        {
+            return _messages.ToList();
+        }
+    }
+}
---
+++ CSharping/CSharping/AsyncTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CSharping
+{
+    [TestFixture]
+    public class AsyncTests
+    {
+        [Test]
+        public async void Async_OneTask_Workflow()
+        {
+            var model = new AsyncModel();
+
+            model.AddMessage("starting main");
+            Task task = SimulateTask("A", 10, model);
+            model.AddMessage("doing independent work");
+            
+            await task;
+
+            var messages = model.GetMessages();
+            Assert.AreEqual("starting main", messages[0]);
+            Assert.AreEqual("starting task A", messages[1]);
+            Assert.AreEqual("doing independent work", messages[2]);
+            Assert.AreEqual("task A ended", messages[3]);
+        }
+
+
+        [Test]
+        public async void Async_TwoTasks_Workflow()
+        {
+            var model = new AsyncModel();
+
+            model.AddMessage("starting main");
+            Task shortTask = SimulateTask("short", 10, model);
+            Task longTask = SimulateTask("long", 20, model);
+
+            model.AddMessage("doing independent work");
+
+            await Task.WhenAll(shortTask, longTask);
+
+            var messages = model.GetMessages();
+            Assert.AreEqual("starting main", messages[0]);
+            Assert.AreEqual("starting task short", messages[1]);
+            Assert.AreEqual("starting task long", messages[2]);
+            Assert.AreEqual("doing independent work", messages[3]);
+            Assert.AreEqual("task short ended", messages[4]);
+            Assert.AreEqual("task long ended", messages[5]);
+        }
+
+        private async Task SimulateTask(string name, int ms, AsyncModel model)
+        {
+            model.AddMessage("starting task {0}", name);
+            await Task.Delay(ms);
+            model.AddMessage("task {0} ended", name);
+        }
+    }
+}
