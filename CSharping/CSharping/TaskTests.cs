cc4f38a4eb97b7a77b9ee32fa646c6130598762e
dominic wyler <dominic.wyler@gmail.com>
2015-01-29 18:46:01 +0100

    added a test case with a continuation

--- CSharping/CSharping/TaskTests.cs
+++ CSharping/CSharping/TaskTests.cs
@@ -182,6 +182,21 @@ namespace CSharping
             }
         }
 
+        [Test]
+        public void Continuation_IsExecutedAfterAntecedent()
+        {
+            var queue = new MessageQueue();
+            Task<string> first = Task.Factory.StartNew(() => DoWork("first", queue));
+            Task<string> continuation = first.ContinueWith(antecedentTask => DoWork("continuation after " + antecedentTask.Result, queue));
+
+            Task.WaitAll(first, continuation);
+
+            var messages = queue.GetAll();
+            Assert.AreEqual("first", messages[0]);
+            Assert.AreEqual("continuation after first", messages[1]);
+        }
+
+
         private string DoWork(string name, MessageQueue queue)
         {
             queue.AddMessage(name);
