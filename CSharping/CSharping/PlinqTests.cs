febc4c1408d2232cf00ed0581c18395efcc4a897
dominic wyler <dominic.wyler@gmail.com>
2015-02-04 08:20:16 +0100

    added a test case with a plinq query

---
+++ CSharping/CSharping/PlinqTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CSharping
+{
+    [TestFixture]
+    public class PlinqTests
+    {
+        [Test]
+        public void AsParallel_Linq()
+        {
+            IEnumerable<int> numbers = Enumerable.Range(1, 100000);
+
+            ParallelQuery<int> parallelQuery =
+              from n in numbers.AsParallel()
+              where n % 2 == 0
+              select n;
+
+            int[] evenNumbers = parallelQuery.ToArray();
+
+            Assert.AreEqual(50000, evenNumbers.Length);
+        }
+    }
+}
