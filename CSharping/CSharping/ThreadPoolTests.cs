6b337042962a8de3cb51a76731bc9b3fb4d19ea9
dominic wyler <dominic.wyler@gmail.com>
2015-02-05 19:45:29 +0100

    added a test case with ThreadPool

---
+++ CSharping/CSharping/ThreadPoolTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace CSharping
+{
+    [TestFixture]
+    public class ThreadPoolTests
+    {
+        [Test]
+        public void QueueUserWorkItem()
+        {
+            ThreadPool.QueueUserWorkItem(state => Console.WriteLine("stuff:" + state));
+            ThreadPool.QueueUserWorkItem(DoWork, 1234);
+            ThreadPool.QueueUserWorkItem(DoWork, "stuff");
+        }
+
+        private void DoWork(object state)
+        {
+
+        }
+    }
+}
