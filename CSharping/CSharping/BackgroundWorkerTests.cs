f2ed637c74dd638312b5a211b0060f5638d1932d
dominic wyler <dominic.wyler@gmail.com>
2015-02-10 08:20:46 +0100

    added a test class for BackgroundWorker

---
+++ CSharping/CSharping/BackgroundWorkerTests.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using NUnit.Framework;
+
+namespace CSharping
+{
+    [TestFixture]
+    public class BackgroundWorkerTests
+    {
+        [Test]
+        public void BackgroundWorker()
+        {
+            var worker = new BackgroundWorker();
+            worker.DoWork += DoWork;
+            worker.RunWorkerCompleted += OnRunWorkerCompleted;
+
+            worker.RunWorkerAsync("work");
+        }
+
+        private void DoWork(object sender, DoWorkEventArgs args)
+        {
+            args.Result = args.Argument + " finished";
+        }
+
+        private void OnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs args)
+        {
+            string result = (string) args.Result;
+            Assert.AreEqual("work finished", result);
+        }
+    }
+}
