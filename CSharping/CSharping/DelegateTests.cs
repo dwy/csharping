036a172bbc7339bbdef96490c77bab52a11c1107
dominic wyler <dominic.wyler@gmail.com>
2015-02-12 07:57:51 +0100

    added a test class for delegates

---
+++ CSharping/CSharping/DelegateTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace CSharping
+{
+    public delegate void MessageActionDelegate(string message);
+    public delegate string MessageFunctionDelegate(string message);
+
+    [TestFixture]
+    public class DelegateTests
+    {
+        [Test]
+        public void CallActionDelegate_PassLambda()
+        {
+            var queue = new MessageQueue();
+            var actionDelegate = new MessageActionDelegate(m => { queue.AddMessage(m); });
+
+            actionDelegate("hi");
+
+            Assert.AreEqual("hi", queue.GetAll()[0]);
+        }
+    }
+}
