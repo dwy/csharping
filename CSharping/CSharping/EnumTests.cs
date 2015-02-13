1d19c83680bf31a9f0ab29531bd8eb434e7ef549
dominic wyler <dominic.wyler@gmail.com>
2015-02-13 08:19:13 +0100

    added a test class for enums

---
+++ CSharping/CSharping/EnumTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CSharping
+{
+    // https://msdn.microsoft.com/en-us/library/cc138362.aspx
+
+    [TestFixture]
+    public class EnumTests
+    {
+        [Test]
+        public void EnumsAreValueTypes()
+        {
+            const Days day = Days.Friday;
+
+            Assert.IsInstanceOf<ValueType>(day);
+        }
+
+        enum Days
+        {
+            Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+        }
+    }
+}
