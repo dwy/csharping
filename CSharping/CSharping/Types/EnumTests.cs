39d235b0873626a209345d86a480d2f0c0f43b26
dominic wyler <dominic.wyler@gmail.com>
2015-02-16 07:58:29 +0100

    added test cases with different start values and underlying types

--- CSharping/CSharping/Types/EnumTests.cs
+++ CSharping/CSharping/Types/EnumTests.cs
@@ -17,18 +17,50 @@ namespace CSharping.Types
         }
 
         [Test]
-        public void DefaultUnderlyingTypeIsInt_StartsAtZero()
+        public void DefaultUnderlyingTypeIsInt_StartsAtZero_CountsUp()
         {
             const int sundayNumber = (int) Days.Sunday;
             const int mondayNumber = (int) Days.Monday;
+            const int saturdayNumber = (int) Days.Saturday;
 
             Assert.AreEqual(0, sundayNumber);
             Assert.AreEqual(1, mondayNumber);
+            Assert.AreEqual(6, saturdayNumber);
         }
 
         enum Days
         {
             Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+        }
+
+        [Test]
+        public void SpecifyMemberValue()
+        {
+            const int noneValue = (int) AddressType.None;
+            const int shipmentValue = (int) AddressType.Shipment;
+            const int billValue = (int) AddressType.Bill;
+
+            Assert.AreEqual(0, noneValue);
+            Assert.AreEqual(3, shipmentValue);
+            Assert.AreEqual(4, billValue);
+        }
+
+        enum AddressType
+        {
+            None = 0, Payment = 2, Shipment, Bill
+        }
+
+        [Test]
+        public void SpecifyUnderlyingType()
+        {
+            const byte codeMonkeyValue = (byte)EmployeeType.CodeMonkey;
+
+            Assert.AreEqual(128, codeMonkeyValue);
+        }
+
+        enum EmployeeType : byte
+        {
+            CodeMonkey = 128, Manager = 255   
         }
     }
 }
