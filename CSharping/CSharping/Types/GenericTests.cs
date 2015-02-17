d9fb52991bfd1d090737f9ed6201da348cd931f9
dominic wyler <dominic.wyler@gmail.com>
2015-02-17 18:55:52 +0100

    type constraint new()

--- CSharping/CSharping/Types/GenericTests.cs
+++ CSharping/CSharping/Types/GenericTests.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 namespace CSharping.Types
 {
     // https://msdn.microsoft.com/en-us/library/sz6zd40f.aspx
+    // https://msdn.microsoft.com/en-us/library/d5x73970.aspx
 
     [TestFixture]
     public class GenericTests
@@ -11,18 +12,38 @@ namespace CSharping.Types
         [Test]
         public void Simple()
         {
-            var g = new GenericClass<string>();
+            var g = new Generic<string>();
 
             Type type = g.GetParameterType();
 
             Assert.AreEqual(typeof (string), type);
         }
 
-        class GenericClass<T>
+        class Generic<T>
         {
             public Type GetParameterType()
             {
                 return typeof(T);
+            }
+        }
+
+        [Test]
+        public void TypeParameterConstraint_New_ParameterlessConstructor()
+        {
+            var newConstraint = new GenericWithNewConstraint<Int32>();
+            // compile error: string has no public parameterless constructor 
+            // var newConstraint2 = new GenericWithNewConstraint<string>();
+
+            Int32 instance = newConstraint.CreateInstance();
+
+            Assert.IsNotNull(instance);
+        }
+
+        class GenericWithNewConstraint<T> where T : new()
+        {
+            public T CreateInstance()
+            {
+                return new T();
             }
         }
     }
