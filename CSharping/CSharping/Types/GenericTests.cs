58bb88b30379754c9e8c852d976590fe98b7258d
dominic wyler <dominic.wyler@gmail.com>
2015-02-17 08:22:35 +0100

    added class for generic tests

---
+++ CSharping/CSharping/Types/GenericTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CSharping.Types
+{
+    // https://msdn.microsoft.com/en-us/library/sz6zd40f.aspx
+
+    [TestFixture]
+    public class GenericTests
+    {
+        [Test]
+        public void Simple()
+        {
+            var g = new GenericClass<string>();
+
+            Type type = g.GetParameterType();
+
+            Assert.AreEqual(typeof (string), type);
+        }
+
+        class GenericClass<T>
+        {
+            public Type GetParameterType()
+            {
+                return typeof(T);
+            }
+        }
+    }
+}
