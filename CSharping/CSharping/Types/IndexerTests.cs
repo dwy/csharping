f205ba2f80b29af1529b7fec33e4c629437d783c
dominic wyler <dominic.wyler@gmail.com>
2015-02-17 07:29:51 +0100

    set indexer example

--- CSharping/CSharping/Types/IndexerTests.cs
+++ CSharping/CSharping/Types/IndexerTests.cs
@@ -18,6 +18,17 @@ namespace CSharping.Types
             Assert.AreEqual(3, list[2]);
         }
 
+        [Test]
+        public void Indexer_Set()
+        {
+            var list = new IndexedIntList(1, 2, 3);
+            list[1] = 20;
+
+            Assert.AreEqual(1, list[0]);
+            Assert.AreEqual(20, list[1]);
+            Assert.AreEqual(3, list[2]); 
+        }
+
         class IndexedIntList
         {
             private readonly List<int> _list = new List<int>();
