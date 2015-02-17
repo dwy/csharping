068e98c53d3117e978e99d5889f67ee1604d83aa
dominic wyler <dominic.wyler@gmail.com>
2015-02-17 07:53:11 +0100

    added class for method tests

---
+++ CSharping/CSharping/Types/MethodTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace CSharping.Types
+{
+    [TestFixture]
+    public class MethodTests
+    {
+        [Test]
+        public void Override_BaseMethodIsReplaced()
+        {
+            var child = new ChildOverride();
+
+            string nameFromOverride = child.GetName();
+            string nameFromBase = ((Base)child).GetName();
+
+            Assert.AreEqual("override", nameFromOverride);
+            Assert.AreEqual("override", nameFromBase);
+        }
+
+        class Base
+        {
+            public virtual string GetName() { return "base"; } 
+        }
+
+        class ChildOverride: Base
+        {
+            public override string GetName() { return "override"; }
+        }
+
+    }
+}
