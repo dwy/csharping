db18540c3ca2b1cb09b01b68355ce954243c32b5
dominic wyler <dominic.wyler@gmail.com>
2015-02-19 08:05:49 +0100

    boxing and unboxing value type

---
+++ CSharping/CSharping/Types/BoxingUnboxingTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace CSharping.Types
+{
+    // https://msdn.microsoft.com/en-us/library/yz2be5wk.aspx
+
+    [TestFixture]
+    public class BoxingUnboxingTests
+    {
+        [Test]
+        public void Boxing_ValidUnboxing()
+        {
+            const int value = 42;
+
+            object boxed = value; // implicitly box integer in an object
+            int unboxed = (int) boxed; // unbox explicitly
+
+            Assert.AreEqual(42, boxed);
+            Assert.AreEqual(42, unboxed);
+            Assert.AreEqual(42, value);
+        }
+    }
+}
