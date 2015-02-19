c595171c2d619e30b3e0b2ffa64f8578978d1b65
dominic wyler <dominic.wyler@gmail.com>
2015-02-19 07:54:41 +0100

    extension method

---
+++ CSharping/CSharping/Types/ExtensionTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace CSharping.Types
+{
+    [TestFixture]
+    public class ExtensionTests
+    {
+        [Test]
+        public void ExtensionMethod()
+        {
+            var employee = new Employee(1);
+
+            decimal salary = employee.GetSalary(1000);
+
+            Assert.AreEqual(2000, salary);
+        }
+    }
+
+    public static class EmployeeExtenstion
+    {
+        public static decimal GetSalary(this Employee employee, decimal bonus)
+        {
+            return employee.Type * 1000 + bonus;
+        }
+    }
+
+    public class Employee
+    {
+        private readonly int _type;
+
+        public Employee(int type)
+        {
+            _type = type;
+        }
+
+        public int Type
+        {
+            get { return _type; }
+        }
+    }
+}
