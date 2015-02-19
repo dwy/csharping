a08a89879a269b47b6c81c1ebc1ec1fce8e5b232
dominic wyler <dominic.wyler@gmail.com>
2015-02-19 08:23:25 +0100

    dynamic tests

---
+++ CSharping/CSharping/Types/DynamicTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+using NUnit.Framework;
+
+namespace CSharping.Types
+{
+    // https://msdn.microsoft.com/en-us/library/dd264736.aspx
+
+    [TestFixture]
+    public class DynamicTests
+    {
+        [Test]
+        [ExpectedException(typeof(RuntimeBinderException))]
+        public void InexistentMethod_CompilesButThrows()
+        {
+            dynamic dyn = new MyClass();
+
+            dyn.InexistentMethod();
+        }
+
+        [Test]
+        public void MethodExists_ResolvedAtRuntime()
+        {
+            dynamic dyn = new MyClass();
+
+            dynamic result = dyn.Get(1);
+
+            Assert.AreEqual(1, result);
+            Assert.IsInstanceOf<int>(result);
+        }
+
+        class MyClass
+        {
+            public string Get(string s)
+            {
+                return s;
+            }
+
+            public int Get(int i)
+            {
+                return i;
+            }
+        }
+    }
+}
