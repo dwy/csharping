22a8c5ff932b827bcc432f57210e873eb203b3ea
dominic wyler <dominic.wyler@gmail.com>
2015-02-19 18:17:25 +0100

    added second dynamic call

--- CSharping/CSharping/Types/DynamicTests.cs
+++ CSharping/CSharping/Types/DynamicTests.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 namespace CSharping.Types
 {
     // https://msdn.microsoft.com/en-us/library/dd264736.aspx
+    // https://msdn.microsoft.com/en-us/library/dd264741.aspx
 
     [TestFixture]
     public class DynamicTests
@@ -23,9 +24,12 @@ namespace CSharping.Types
             dynamic dyn = new MyClass();
 
             dynamic result = dyn.Get(1);
+            dynamic result2 = dyn.Get("value");
 
             Assert.AreEqual(1, result);
             Assert.IsInstanceOf<int>(result);
+            Assert.AreEqual("value", result2);
+            Assert.IsInstanceOf<string>(result2);
         }
 
         class MyClass
