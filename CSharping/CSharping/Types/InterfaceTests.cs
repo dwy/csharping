5b66807826cd8c35a99c82b7668a56ab4ef21820
dominic wyler <dominic.wyler@gmail.com>
2015-02-20 07:18:29 +0100

    explicit interface implementation

---
+++ CSharping/CSharping/Types/InterfaceTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+
+namespace CSharping.Types
+{
+    [TestFixture]
+    public class InterfaceTests
+    {
+        [Test]
+        public void InterfaceImplementation()
+        {
+            var impl = new FormattableImplementation();
+
+            string result = impl.ToString("hi", CultureInfo.InvariantCulture);
+
+            Assert.AreEqual(result, "hi");
+        }
+
+        class FormattableImplementation : IFormattable
+        {
+            public string ToString(string format, IFormatProvider formatProvider)
+            {
+                return format;
+            }
+        }
+
+        [Test]
+        public void ExplicitInterfaceImplementation_InstanceMustBeCast()
+        {
+            var impl = new FormattableExplicitImplementation();
+
+            // compile error: method does not exist 
+            // string result = impl.ToString("hi", CultureInfo.InvariantCulture);
+            string result = ((IFormattable)impl).ToString("hi", CultureInfo.InvariantCulture);
+
+            Assert.AreEqual(result, "hi");
+        }
+
+        class FormattableExplicitImplementation : IFormattable
+        {
+            // explicit IFormattabe implementation
+            string IFormattable.ToString(string format, IFormatProvider formatProvider)
+            {
+                return format;
+            }
+        }
+    }
+}
