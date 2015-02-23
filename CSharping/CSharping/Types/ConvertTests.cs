5713e4d63d0594de76591c6945ae18df4af740eb
dominic wyler <dominic.wyler@gmail.com>
2015-02-23 17:58:13 +0100

    convert example with a number

---
+++ CSharping/CSharping/Types/ConvertTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CSharping.Types
+{
+    [TestFixture]
+    public class ConvertTests
+    {
+        [Test]
+        public void UNIT_SCENARIO_RESULT()
+        {
+            const string number = "1";
+
+            int integer = (int) Convert.ChangeType(number, typeof (int));
+
+            Assert.AreEqual(1, integer);
+        }
+    }
+}
