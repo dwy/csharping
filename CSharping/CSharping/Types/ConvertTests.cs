0f898c8d6172a1bb4709978a748e44ef719f506b
dominic wyler <dominic.wyler@gmail.com>
2015-02-24 07:22:07 +0100

    custom convertible class

--- CSharping/CSharping/Types/ConvertTests.cs
+++ CSharping/CSharping/Types/ConvertTests.cs
@@ -68,5 +68,115 @@ namespace CSharping.Types
             Assert.AreEqual('2', c);
             Assert.AreEqual(123, sByte);
         }
+
+        [Test]
+        public void CustomConvertibleClass()
+        {
+            var value = new CustomConvertibleValue(42);
+
+            byte b = Convert.ToByte(value);
+            char c = Convert.ToChar(value);
+            decimal d = Convert.ToDecimal(value);
+
+            Assert.AreEqual(42, b);
+            Assert.AreEqual('*', c);
+            Assert.AreEqual(42m, d);
+            Assert.Throws<InvalidCastException>(() => Convert.ToDateTime(value));
+        }
+
+        class CustomConvertibleValue : IConvertible
+        {
+            private readonly int _value;
+
+            public CustomConvertibleValue(int value)
+            {
+                _value = value;
+            }
+
+            public TypeCode GetTypeCode()
+            {
+                return TypeCode.Object;
+            }
+
+            public bool ToBoolean(IFormatProvider provider)
+            {
+                return _value > 0;
+            }
+
+            public char ToChar(IFormatProvider provider)
+            {
+                return (char) _value;
+            }
+
+            public sbyte ToSByte(IFormatProvider provider)
+            {
+                return (sbyte) _value;
+            }
+
+            public byte ToByte(IFormatProvider provider)
+            {
+                return (byte) _value;
+            }
+
+            public short ToInt16(IFormatProvider provider)
+            {
+                return (Int16) _value;
+            }
+
+            public ushort ToUInt16(IFormatProvider provider)
+            {
+                return (ushort) _value;
+            }
+
+            public int ToInt32(IFormatProvider provider)
+            {
+                return _value;
+            }
+
+            public uint ToUInt32(IFormatProvider provider)
+            {
+                return (uint) _value;
+            }
+
+            public long ToInt64(IFormatProvider provider)
+            {
+                return (long) _value;
+            }
+
+            public ulong ToUInt64(IFormatProvider provider)
+            {
+                return (ulong) _value;
+            }
+
+            public float ToSingle(IFormatProvider provider)
+            {
+                return (float) _value;
+            }
+
+            public double ToDouble(IFormatProvider provider)
+            {
+                return (double) _value;
+            }
+
+            public decimal ToDecimal(IFormatProvider provider)
+            {
+                return (decimal) _value;
+            }
+
+            public DateTime ToDateTime(IFormatProvider provider)
+            {
+                throw new InvalidCastException();
+            }
+
+            public string ToString(IFormatProvider provider)
+            {
+                return _value.ToString(provider);
+            }
+
+            public object ToType(Type conversionType, IFormatProvider provider)
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
