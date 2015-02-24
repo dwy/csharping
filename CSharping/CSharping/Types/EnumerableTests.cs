085f83c3a4c579ef0f8317afd42a8862851ad936
dominic wyler <dominic.wyler@gmail.com>
2015-02-24 17:40:15 +0100

    basic IEnumerable implementation

---
+++ CSharping/CSharping/Types/EnumerableTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CSharping.Types
+{
+    [TestFixture]
+    public class EnumerableTests
+    {
+        [Test]
+        public void IEnumerable_YieldReturn_CompilerGeneratesEnumerator()
+        {
+            var counter = new CountTo(3);
+
+            var result = counter.Cast<int>().ToList();
+
+            Assert.AreEqual(0, result[0]);
+            Assert.AreEqual(1, result[1]);
+            Assert.AreEqual(2, result[2]);
+        }
+
+        class CountTo : IEnumerable
+        {
+            private readonly int _count;
+
+            public CountTo(int count)
+            {
+                _count = count;
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                for (int i = 0; i <= _count; i++) // compiler generates enumerator
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+}
