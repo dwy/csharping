95cbf51321cac28cb363574a7bb73d29b389fcbd
dominic wyler <dominic.wyler@gmail.com>
2015-02-24 07:45:53 +0100

    generic IComparable implementation

--- CSharping/CSharping/Types/ComparableTests.cs
+++ CSharping/CSharping/Types/ComparableTests.cs
@@ -49,5 +49,45 @@ namespace CSharping.Types
                 return String.Compare(Value, c.Value, StringComparison.OrdinalIgnoreCase);
             }
         }
+
+        [Test]
+        public void GenericComparable_SortList()
+        {
+            var values = new List<GenericComparable>
+            {
+                new GenericComparable(22), new GenericComparable(11), new GenericComparable(33), new GenericComparable(11)
+            };
+
+            values.Sort();
+
+            Assert.AreEqual(11, values[0].Value);
+            Assert.AreEqual(11, values[1].Value);
+            Assert.AreEqual(22, values[2].Value);
+            Assert.AreEqual(33, values[3].Value);
+        }
+
+        class GenericComparable : IComparable<GenericComparable>
+        {
+            private readonly int _value;
+
+            public GenericComparable(int value)
+            {
+                _value = value;
+            }
+
+            public int Value
+            {
+                get { return _value; }
+            }
+
+            public int CompareTo(GenericComparable other)
+            {
+                if (other == null) return 1;
+                if (_value > other._value) return 1;
+                if (_value < other._value) return -1;
+                return 0;
+                // equivalent: return _value.CompareTo(other._value);
+            }
+        }
     }
 }
