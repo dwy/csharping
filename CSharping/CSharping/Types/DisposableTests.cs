d38161181f0a32c00ff23d6fce0560396015fa6c
dominic wyler <dominic.wyler@gmail.com>
2015-02-25 07:42:25 +0100

    basic disposable example

---
+++ CSharping/CSharping/Types/DisposableTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CSharping.Types
+{
+    // https://msdn.microsoft.com/en-us/library/system.idisposable%28v=vs.110%29.aspx
+
+    [TestFixture]
+    public class DisposableTests
+    {
+        [Test]
+        public void SimpleDisposable_Using_CallsDispose()
+        {
+            SimpleDisposable simpleDisposable;
+            using (simpleDisposable = new SimpleDisposable())
+            {
+                // do stuff
+            }
+
+            Assert.IsTrue(simpleDisposable.WasDisposed);
+        }
+
+        class SimpleDisposable : IDisposable
+        {
+            public void Dispose()
+            {
+                // free unmanaged resources
+
+                // free managed resources
+
+                WasDisposed = true;
+            }
+
+            public bool WasDisposed { get; private set; }
+        }
+    }
+}
