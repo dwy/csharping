b3093853aa0d94d30deed51706a6d67748d07e62
dominic wyler <dominic.wyler@gmail.com>
2015-02-25 07:59:19 +0100

    disposable pattern

--- CSharping/CSharping/Types/DisposableTests.cs
+++ CSharping/CSharping/Types/DisposableTests.cs
@@ -33,5 +33,52 @@ namespace CSharping.Types
 
             public bool WasDisposed { get; private set; }
         }
+
+        [Test]
+        public void DisposablePattern_Using_CallsDispose()
+        {
+            DisposablePattern disposable;
+            using (disposable = new DisposablePattern())
+            {
+                // do stuff
+            }
+
+            Assert.IsTrue(disposable.WasDisposed);
+        }
+
+        class DisposablePattern : IDisposable
+        {
+            public bool WasDisposed { get; private set; }
+
+            public void Dispose()
+            {
+                Dispose(true);
+                // do not call finalizer on this object, resources already cleaned up by Dispose(true).
+                GC.SuppressFinalize(this);
+            }
+
+            // parameter 'disposing' true means that it is safe to clean up the managed resources too.
+            protected virtual void Dispose(bool disposing)
+            {
+                if (WasDisposed) return;
+
+                if (disposing)
+                {
+                    // free managed resources
+                }
+
+                // free unmanaged resources
+
+                WasDisposed = true;
+            }
+
+            // finalizer
+            ~DisposablePattern()
+            {
+                // when the finalizer is called, the managed resources might already have been cleaned up
+                // by the garbage collector, hence calling Dispose with false.
+                Dispose(false);
+            }
+        }
     }
 }
